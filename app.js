f7e78939a0e3da2cbe236a5b43385564bf4fa4d9
YmChoi-Repo <ammin1127@naver.com>
2022-12-26 22:39:35 +0900

    참, 거짓에 따른 js 제어문

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const a = 5;
-const b = 2;
-//const, let, var = 선언 함수 - 그러나 variable을 업데이트 하고 싶으면 let을 씀. 아니면 const~
-//var은 어떠한 규칙도 없음(밑에 함수가 어떻게 바뀔질 모름)
-let myName = "ym";
+// const amiFat = "true";
+//이것은 문자선언이오
+const amiFat = true;
+console.log(amiFat);
 
-
-console.log(5 + 2);
-console.log(5 / 2);
-console.log(5 - 2);
-console.log("hello " + ym);
-
-myName = "nico";
-console.log("my name is changed" + myName + "this");
+const amislim = false;
+console.log(amislim);
