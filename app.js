6fc7e9184de5f59d1c6ddb9bef05f363108dfb2d
YmChoi-Repo <ammin1127@naver.com>
2022-12-26 22:21:51 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const a = 5;
+const b = 2;
+//const, let, var = 선언 함수 - 그러나 variable을 업데이트 하고 싶으면 let을 씀. 아니면 const~
+//var은 어떠한 규칙도 없음(밑에 함수가 어떻게 바뀔질 모름)
+let myName = "ym";
+
+
+console.log(5 + 2);
+console.log(5 / 2);
+console.log(5 - 2);
+console.log("hello " + ym);
+
+myName = "nico";
+console.log("my name is changed" + myName + "this");
